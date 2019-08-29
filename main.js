e5dfd035e3f4104fa8f3e0daa0c95474774b1ef4
Michael Torres <michaelj.torres@yahoo.com>
2019-08-28 20:05:50 -0400

    all values added.. css added.. working on JS values

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-let operation1 = null;
-let operation2 = null;
-let numArrayStore = [];
+var oneButton = document.getElementById('one');
+var twoButton = document.getElementById('two');
+var threeButton = document.getElementById('three');
+var fourButton = document.getElementById('four');
+var fiveButton = document.getElementById('five');
+var sixButton = document.getElementById('six');
+var sevenButton = document.getElementById('seven');
+var eightButton = document.getElementById('eight');
+var nineButton = document.getElementById('nine');
+var zeroButton = document.getElementById('zero');
 
-// values will change in function when turned true.
-let add = false;
-let subtract = false
-let multiply = false;
-let divide = false; 
 
-display = () => {
-    document.getElementById('display').value += value;
-}
-display = (value) => {
-    document.getElementById("display").value += value;
-}
+var oneButton = document.getElementById('decimal');
+var oneButton = document.getElementById('clear');
+var oneButton = document.getElementById('plusMinus');
+var oneButton = document.getElementById('percent');
 
-document.getElementById("clear").addEventListener("click", function () {
-    location.reload();
-  }, false);
 
+var oneButton = document.getElementById('add');
+var oneButton = document.getElementById('subtract');
+var oneButton = document.getElementById('multiply');
+var oneButton = document.getElementById('divide');
+var oneButton = document.getElementById('equal');
+
+var oneButton = document.getElementById('display');
 
 
