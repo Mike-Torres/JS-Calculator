4bf673cdd953a1c77cd202e2960f53386c7f1d70
Michael Torres <michaelj.torres@yahoo.com>
2019-08-30 17:28:24 -0400

    changing JS functionality

--- main.js
+++ main.js
@@ -1,45 +1,39 @@
-var oneButton = document.getElementById('one');
-var twoButton = document.getElementById('two');
-var threeButton = document.getElementById('three');
-var fourButton = document.getElementById('four');
-var fiveButton = document.getElementById('five');
-var sixButton = document.getElementById('six');
-var sevenButton = document.getElementById('seven');
-var eightButton = document.getElementById('eight');
-var nineButton = document.getElementById('nine');
-var zeroButton = document.getElementById('zero');
-
-var decimalButton = document.getElementById('decimal');
-var clearButton = document.getElementById('clear');
-var plusMinusButton = document.getElementById('plusMinus');
-var percentButton = document.getElementById('percent');
-
-var addButton = document.getElementById('add');
-var subtractButton = document.getElementById('subtract');
-var multiplyButton = document.getElementById('multiply');
-var divideButton = document.getElementById('divide');
-var equalButton = document.getElementById('equal');
-var displayValElement = document.getElementById('display');
-
-var displayVal = '0';
-var pendingVal;
-var evalStringArray = [];
-
-var allButtons = document.getElementsByClassName('btn-num');
-var allOperations = document.getElementsByClassName('btn-ops');
-
-var updateDisplayVal = (clickObj) => {
-    var btnText = clickObj.target.innerText;
-
-    if (displayVal === '0')
-        displayVal = '';
-
-    displayVal += btnText;
-    displayValElement.innerText = displayVal;
+inputValue = (num) => {
+    var input_var = document.getElementById('display');
+    switch (num) {
+        case 0:
+            input_var.value += "0";
+            break;
+        case 1:
+            input_var.value += "1";
+            break;
+        case 2:
+            input_var.value += "2";
+            break;
+        case 3:
+            input_var.value += "3";
+            break;
+        case 4:
+            input_var.value += "4";
+            break;
+        case 5:
+            input_var.value += "5";
+            break;
+        case 6:
+            input_var.value += "6";
+            break;
+        case 7:
+            input_var.value += "7";
+            break;
+        case 8:
+            input_var.value += "8";
+            break;
+        case 9:
+            input_var.value += "9";
+            break;
+    }
 }
-for (var i = 0; i < allButtons.length; i++) {
-    allButtons[i].addEventListener('click', updateDisplayVal, false);
+
+clear = () => {
+    document.getElementById('display').value = ''
 }
-// for (var i = 0; i <= allOperations.length; i++) {
-//     allOperations[i].addEventListener(click, completeOperation, false)
-// }
