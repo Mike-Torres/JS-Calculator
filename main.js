03a1d6d9741a2ee5b6b7eb1a998d16be17f0f39d
Michael Torres <michaelj.torres@yahoo.com>
2019-09-04 23:14:45 -0400

    removed all pseudo code

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 const reg = /[+|*|\-|/]$/;
 const regToken = /(\d+\.\d+|\d+\.|\d+|\+|\-|\*|\/)$/;
 
-// Source of truth
-// This is the state in Angular, React, Vue, etc
 var currentFormula = '';
 var isLastButtonPressedEqualSign = false;
 
-// Dependent on source-of-truth
-// Angular, React, Vue's built in way of rendering HTML
 var currentValue = '';
 const displayFormula = document.querySelector('#current-formula');
 const displayValue = document.querySelector('#current-value');
@@ -20,20 +16,17 @@ const inputValue = (param) => {
     if (currentFormula === '0') {
         currentFormula = '';
     }
-    currentFormula = currentFormula + param; // business-logic
+    currentFormula = currentFormula + param; 
     renderView(param);
 }
 
 const inputOperand = (param) => {
-    if (!currentFormula.match(reg)) { // if it's not any of these in the regular expression then run it
-        currentFormula = currentFormula + param; // business-logic
+    if (!currentFormula.match(reg)) { 
+        currentFormula = currentFormula + param; 
         renderView(param);
-        // side-effect
     } else {
-        // last character is an operand
         currentFormula = currentFormula.slice(0, -1) + param;
         renderView(param);
-        // side-effect
     }
 }
 
@@ -67,8 +60,6 @@ const doComputation = () => {
 }
 
 const changeSign = (param) => {
-    // If currentFormula and currentValue equal, change both signs
-    // Disallow multiple - in currentFormula
     if(currentFormula[0] !== '-'){
         currentFormula = '-' + currentFormula;
     } else {
@@ -87,122 +78,8 @@ const renderView = () => {
 
         currentFormula = currentValue;
     } else {
-        currentValue = regToken.exec(currentFormula)[0]; // else run this
+        currentValue = regToken.exec(currentFormula)[0];
         displayFormula.innerHTML = currentFormula;
         displayValue.innerHTML = currentValue;
     }
 }
-
-
-
-
-// //--------------------------------------input to display---------------------------------------------------
-// inputValue = (num) => {
-//     var numberValue = document.getElementById('display');
-//     switch (num) {
-//         case 0:
-//             numberValue.value += "0";
-//             break;
-//         case 1:
-//             numberValue.value += "1";
-//             break;
-//         case 2:
-//             numberValue.value += "2";
-//             break;
-//         case 3:
-//             numberValue.value += "3";
-//             break;
-//         case 4:
-//             numberValue.value += "4";
-//             break;
-//         case 5:
-//             numberValue.value += "5";
-//             break;
-//         case 6:
-//             numberValue.value += "6";
-//             break;
-//         case 7:
-//             numberValue.value += "7";
-//             break;
-//         case 8:
-//             numberValue.value += "8";
-//             break;
-//         case 9:
-//             numberValue.value += "9";
-//             break;
-//     }
-// }
-
-// //--------------------------------------Operations---------------------------------------------------
-// inputOperand = (operand) => {
-//     var operandisPressed = false;
-//     var operation = document.getElementById('display');
-//     switch (operand) {
-//         case '+':
-//             operation.value += "+";
-//             break;
-//         case '-':
-//             operation.value += "-";
-//             break;
-//         case "x":
-//             operation.value += "*";
-//             break;
-//         case '/':
-//             operation.value += "/";
-//             break;
-//         case '+/-':
-//             operation.value += "-" + operation.value;
-//             break;
-//         case '.':
-//             if (operandisPressed) {
-//                 operandisPressed === true
-//                 operation.value += ".";
-//             }
-//             return operation.value += "."
-//     }
-// }
-// //--------------------------------------Non-Operands---------------------------------------------------
-// backSpace = () => {
-//     var backSpace = document.getElementById('display');
-//     var removeLast = backSpace.value;
-//     if (removeLast > 0) {
-//         removeLast = removeLast.substring(0, removeLast.length - 1);
-//         backSpace.value = removeLast;
-//     }
-// }
-// backSpace();
-
-// clear = () => {
-//     document.getElementById("clear").addEventListener("click", () => {
-//         document.getElementById('display').value = '';
-//     }, false);
-// }
-// clear();
-
-// computation = () => {
-//     var computationValue = document.getElementById("display")
-//     result = eval(computationValue.value)
-//     document.getElementById('display').value = result.toFixed(2);
-// }
-
-
-
-// function addDecimal () {
-//     var currentNumber = document.getElementById('display');
-//     if
-
-// }
-
-// handleDecimal = () => {
-//     let decimalPoint = false;
-//     console.log(decimalPoint);
-//     if (decimalPoint === null) {
-//         document.getElementById('decimal').value = '.';
-//         console.log(decimalPoint)
-//     } else {
-//         decimalPoint === true;
-//         console.log(decimalPoint);
-//         document.getElementById('decimal').value = '';
-//         console.log(decimalPoint);
-//     }
-// }
